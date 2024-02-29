fcad6e527277b7cf19ef81f99bc84e6868e63d67
Otávio Maldaner <otaviomaldaner571@gmail.com>
2024-02-29 20:46:57 -0300

    upload

---
+++ Exercícios Python Brasil/Estrutura de Decisão/exercicio1.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+maior = max(num1, num2)
+print(f"O maior numero é {maior}")
